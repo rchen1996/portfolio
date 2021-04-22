72e0b24b9ad1c20020669ddf39221b23d3809fda
Rebecca Chen <rebecca061996@gmail.com>
2021-04-22 19:17:13 -0400

    update nav component to use link rather than anchor

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,20 @@
+import Link from 'react-router-dom';
+
 export default function Nav(props) {
   return (
     <nav>
       <ul>
         <li>
-          <a href='#'>Home</a>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <a href='#'>About</a>
+          <Link to='/about'>About</Link>
         </li>
         <li>
-          <a href='#'>My Work</a>
+          <Link to='/work'>My Work</Link>
         </li>
         <li>
-          <a href='#'>Contact</a>
+          <Link to='/contact'>Contact</Link>
         </li>
       </ul>
     </nav>
