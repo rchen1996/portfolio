a3512e4a334f57e0660463e70a4cc9fa385c3b58
Rebecca Chen <rebecca061996@gmail.com>
2021-04-22 19:07:08 -0400

    create components folder and create component file for header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(props) {
+  return (
+    <header>
+      <div>
+        <img alt='' />
+      </div>
+      <button aria-label='toggle navigation'>
+        <span></span>
+      </button>
+    </header>
+  );
+}
