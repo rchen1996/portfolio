780f696f588d6d7ce559dd9e8c11920b094ddf8f
Rebecca Chen <rebecca061996@gmail.com>
2021-04-22 19:13:58 -0400

    import react router for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
+    <Router>
       <Header />
-    </div>
+    </Router>
   );
 }
 
