9020082c1c58117b3c975afe0e283860c3131865
Rebecca Chen <rebecca061996@gmail.com>
2021-04-22 19:15:39 -0400

    add switch with routes for each of the 4 pages for portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Header from './components/Header';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Header />
+      <Switch>
+        <Route path='/about'></Route>
+        <Route path='/work'></Route>
+        <Route path='/contact'></Route>
+        <Route path='/'></Route>
+      </Switch>
     </Router>
   );
 }
