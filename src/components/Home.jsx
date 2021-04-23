05ae4d4c08b83a220c768b2bd80e259895a9829f
Rebecca Chen <rebecca061996@gmail.com>
2021-04-22 20:55:59 -0400

    add introduction section and setup section for my skills/about me

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,33 @@
+import { Link } from 'react-router-dom';
+
 export default function Home(props) {
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <h1>
+          Hi, I am <strong>Rebecca Chen</strong>
+        </h1>
+        <p>Full-stack Developer</p>
+      </section>
+
+      <section>
+        <h2>My Skills</h2>
+        <div>
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+        <Link>Learn More</Link>
+      </section>
+    </div>
+  );
 }
